1aef7136647e877818e7fc0191a48bdff55edb2a
andrew1337 <duha_95@mail.ru>
2023-12-28 01:53:23 +0100

    lint lint lint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Dice Paradise',
-  description: 'A dice game with a twist',
-}
+  title: "Dice Paradise",
+  description: "A dice game with a twist",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-    <body suppressHydrationWarning>{children}</body>
+      <body suppressHydrationWarning>{children}</body>
     </html>
-  )
+  );
 }
